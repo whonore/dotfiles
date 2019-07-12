9addfb3ce7a9023b8d283b9cdc539970a58054f4
whonore <wolf.honore@sifive.com>
2019-07-12 11:10:28 -0700

    Allow vim.nix to install different vim versions

--- nix/vim.nix
+++ nix/vim.nix
@@ -1,11 +1,25 @@
-{ pkgs ? import <nixpkgs> {}, py ? "3" }:
+{ pkgs ? import <nixpkgs> {}, py ? "3", vim ? "8.0" }:
 with pkgs;
 
 let
-  pyvars = {
+  pyVars = {
     "2" = { ver = ""; notVer = "3"; py = python27; };
     "3" = { ver = "3"; notVer = ""; py = python36; };
   }.${py};
+  vimVars = {
+    "7.4" = {
+      patch = "2367";
+      sha256 = "1r3a3sh1v4q2mc98j2izz9c5qc1a97vy49nv6644la0z2m92vyik";
+    };
+    "8.0" = {
+      patch = "1850";
+      sha256 = "16n3685gnc2y25kvac4x4bbxsxb1hxvq4p085qzc86lnaakhmmk0";
+    };
+    "8.1" = {
+      patch = "1670";
+      sha256 = "167zqdgvinw80n16cq8jrcgii677c1v9lhjyjnjrmp9gshlihbxb";
+    };
+  }.${vim};
 in stdenv.mkDerivation {
   name = "vim-py${py}";
 
@@ -13,21 +27,21 @@ in stdenv.mkDerivation {
     priority = 10 - builtins.fromJSON py;
   };
 
-  src = fetchurl {
-    url = https://github.com/vim/vim/archive/v8.0.1146.tar.gz;
-    sha256 = "6ef5ada3970bf408239ab23edc5cdb0f815c9ed1b1d24363928c8b21ab4108c9";
+  src = with vimVars; fetchTarball {
+    url = "https://github.com/vim/vim/archive/v${vim}.${patch}.tar.gz";
+    inherit sha256;
   };
 
-  buildInputs = [ncurses xorg.libX11 xorg.libXt pyvars.py];
+  buildInputs = [ncurses xorg.libX11 xorg.libXt pyVars.py];
 
   configureFlags = [
     "--with-features=huge"
     "--enable-cscope=yes"
     "--enable-multibyte=yes"
 
-    "--enable-python${pyvars.ver}interp=yes"
-    "--with-python${pyvars.ver}-config-dir=${pyvars.py}/lib"
-    "--disable-python${pyvars.notVer}interp"
+    "--enable-python${pyVars.ver}interp=yes"
+    "--with-python${pyVars.ver}-config-dir=${pyVars.py}/lib"
+    "--disable-python${pyVars.notVer}interp"
 
     "--with-x"
     "--x-includes=${xlibs.libX11.dev}/include"
