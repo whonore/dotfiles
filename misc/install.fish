6719a9ced116b3cd9d786be28e344f949f5e931f
whonore <wolfhonore@gmail.com>
2019-08-24 15:43:42 -0700

    Add more misc files

--- misc/install.fish
+++ misc/install.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
 set -l dir (dirname (status filename))
-set -l files ".ghci,$HOME/.ghci"
+set -l files ".ghci,$HOME/.ghci" ".vintrc.yaml,$HOME/.vintrc.yaml"
 source $dir/../helper.fish
 install_dots $dir $files
