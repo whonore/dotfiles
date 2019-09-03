a297a4546f41094227b74027afd6ef6bc1bdb99d
whonore <wolfhonore@gmail.com>
2019-09-03 13:29:02 -0400

    Make install.fish executable

--- xmonad/install.fish
+++ xmonad/install.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
 set -l dir (dirname (status filename))
-set -l files ".gitconfig,$HOME/.gitconfig" "ignore,$HOME/.config/git/ignore"
+set -l files ".,$HOME/.xmonad" ".xmobarrc,$HOME/.xmobarrc"
 source $dir/../helper.fish
 install_dots $dir $files
