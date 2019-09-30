63602d1d70a1e0c784b66e556fc6a343ffe15a90
whonore <wolfhonore@gmail.com>
2019-09-30 11:50:15 -0400

    Only run iscaps if xset works

--- fish/functions/iscaps.fish
+++ fish/functions/iscaps.fish
@@ -1,3 +1,7 @@
 function iscaps
-    string match -r -q "\s*on" (xset q | grep Caps | cut -d: -f3)
+    if xset q > /dev/null 2>&1
+        string match -r -q "\s*on" (xset q | grep Caps | cut -d: -f3)
+    else
+        false
+    end
 end
