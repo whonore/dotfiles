41044a1a214b09890db753450c56823a2b027fb2
whonore <wolfhonore@gmail.com>
2020-03-23 14:43:59 -0400

    Add .screenrc

--- misc/install.fish
+++ misc/install.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
 set -l dir (dirname (status filename))
-set -l files ".ghci,$HOME/.ghci" ".vintrc.yaml,$HOME/.vintrc.yaml"
+set -l files ".ghci,$HOME/.ghci" ".vintrc.yaml,$HOME/.vintrc.yaml" ".screenrc,$HOME/.screenrc"
 source $dir/../helper.fish
 install_dots $dir $files
