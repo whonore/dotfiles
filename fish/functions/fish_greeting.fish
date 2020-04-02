ad62d290486ce17b72f9eeb93b42ca148c40a9f5
whonore <wolfhonore@gmail.com>
2020-04-02 15:26:40 -0400

    Update proofaday start

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,5 +1,8 @@
 if command -qs proofaday; and python -m pip freeze | grep -q proofaday
     function fish_greeting
+        if not dproofaday status -q
+            dproofaday start -q; dproofaday status -w -q
+        end
         proofaday
     end
 end
