4cc468c59c9570987bb0f428f568daa3a9c3dc7f
whonore <wolfhonore@gmail.com>
2020-04-03 11:01:28 -0400

    Add proofaday completions

---
+++ fish/completions/dproofaday.fish
@@ -0,0 +1,1 @@
+complete --no-files --command dproofaday --arguments "(env _DPROOFADAY_COMPLETE=complete_fish COMP_WORDS=(commandline -cp) COMP_CWORD=(commandline -t) dproofaday)";
---
+++ fish/completions/proofaday.fish
@@ -0,0 +1,1 @@
+complete --no-files --command proofaday --arguments "(env _PROOFADAY_COMPLETE=complete_fish COMP_WORDS=(commandline -cp) COMP_CWORD=(commandline -t) proofaday)";
