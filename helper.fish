8b8d2ee3d7a8330188840205ff6b438ddaf7b778
whonore <wolfhonore@gmail.com>
2020-05-22 10:42:55 -0400

    Configure bat

--- helper.fish
+++ helper.fish
@@ -2,7 +2,7 @@ function install_dots
     set -l dir $argv[1]
     for arg in $argv[2..-1]
         set -l arg (string split , $arg)
-        set -l src (readlink -e $dir/$arg[1])
+        set -l src (readlink -f $dir/$arg[1])
         set -l dst $arg[2]
 
         if not test -d (dirname $dst)
--- misc/install.fish
+++ misc/install.fish
@@ -1,6 +1,9 @@
 #!/usr/bin/env fish
 
 set -l dir (dirname (status filename))
-set -l files ".ghci,$HOME/.ghci" ".vintrc.yaml,$HOME/.vintrc.yaml" ".screenrc,$HOME/.screenrc" ".fdignore,$HOME/.fdignore"
+set -l files ".ghci,$HOME/.ghci" ".vintrc.yaml,$HOME/.vintrc.yaml" ".screenrc,$HOME/.screenrc" ".fdignore,$HOME/.fdignore" "$dir/../vim/bundle/vim-blueper/tmTheme/Blueper.tmTheme,$HOME/.config/bat/themes/Blueper.tmTheme" "batconfig,$HOME/.config/bat/config"
 source $dir/../helper.fish
 install_dots $dir $files
+if command -qs bat
+    bat cache --build
+end
