8d9b2e76fecbc76ee38d32f0ef9f84b5a6db7ac0
whonore <wolfhonore@gmail.com>
2020-09-23 11:56:27 -0400

    Clean up fish config

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,4 +1,4 @@
-if command -qs proofaday; and python -m pip freeze | grep -q proofaday
+if command -qs proofaday
     function fish_greeting
         if not proofaday 2> /dev/null
             dproofaday -q start; dproofaday -q status -w
