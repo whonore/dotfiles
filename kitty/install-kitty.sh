f3725cd9c05a97a2e1a735cba9f6d5d959a16e67
whonore <wolfhonore@gmail.com>
2020-10-07 16:29:11 -0400

    Change KITTY_HOME to KITTY_ROOT

--- kitty/install-kitty.sh
+++ kitty/install-kitty.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-KITTY_HOME=$HOME/.local/kitty.app
+KITTY_ROOT=$HOME/.local/kitty.app
 mkdir -p ~/.local/bin
 mkdir -p ~/.local/share/applications
 mkdir -p ~/.local/man/man1
 
-ln -s $KITTY_HOME/bin/kitty ~/.local/bin
-cp -n $KITTY_HOME/share/applications/kitty.desktop $KITTY_HOME/share/applications/kitty.desktop.bk
-sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" $KITTY_HOME/share/applications/kitty.desktop
-ln -s $KITTY_HOME/share/applications/kitty.desktop ~/.local/share/applications
-ln -s $KITTY_HOME/share/man/man1/kitty.1 ~/.local/man/man1
+ln -s $KITTY_ROOT/bin/kitty ~/.local/bin
+cp -n $KITTY_ROOT/share/applications/kitty.desktop $KITTY_ROOT/share/applications/kitty.desktop.bk
+sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" $KITTY_ROOT/share/applications/kitty.desktop
+ln -s $KITTY_ROOT/share/applications/kitty.desktop ~/.local/share/applications
+ln -s $KITTY_ROOT/share/man/man1/kitty.1 ~/.local/man/man1
