a9a7d1ad135460112de79a8cf3b811e9df10f405
whonore <wolfhonore@gmail.com>
2020-10-09 09:11:11 -0400

    Add pdfjs

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -874,7 +874,7 @@ c.content.javascript.enabled = True
 ## still be downloaded by clicking the download button in the pdf.js
 ## viewer.
 ## Type: Bool
-# c.content.pdfjs = False
+c.content.pdfjs = True
 
 ## Allow websites to request persistent storage quota via
 ## `navigator.webkitPersistentStorage.requestQuota`.
--- qutebrowser/install-qutebrowser.sh
+++ qutebrowser/install-qutebrowser.sh
@@ -4,5 +4,6 @@ QUTEBROWSER_ROOT=$HOME/.qutebrowser
 cd $QUTEBROWSER_ROOT
 python scripts/mkvenv.py
 $QUTEBROWSER_ROOT/.venv/bin/python3 -m pip install -r ~/.dotfiles/qutebrowser/requirements.txt
+$QUTEBROWSER_ROOT/.venv/bin/python3 $QUTEBROWSER_ROOT/scripts/dev/update_3rdparty.py
 install -Dm755 -t ~/.local/share/qutebrowser/userscripts/ $QUTEBROWSER_ROOT/misc/userscripts/*
 sed -i "s|#!/usr/bin/env python.*|#!$QUTEBROWSER_ROOT/.venv/bin/python3|" ~/.local/share/qutebrowser/userscripts/*
