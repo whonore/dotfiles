a4457069fec7dd79c9b8f35a303353c9efe89ad3
whonore <wolfhonore@gmail.com>
2021-07-13 16:42:05 -0400

    Use fd for coqclean if possible

--- fish/functions/coqclean.fish
+++ fish/functions/coqclean.fish
@@ -14,15 +14,29 @@ function coqclean --description 'Delete Coq compilation files'
     set -l exts glob vo vok vos time aux
     set -l extpat (string join '|' $exts)
     set -l extpat_esc (string join '\|' $exts)
-    set -l pat "\.?.*\.\($extpat_esc\)"
-    for d in $argv
-        set -l fs
-        if set -q _flag_depth
-            set fs (find $d -maxdepth $_flag_depth -regex $pat)
-        else
-            set fs (find $d -regex $pat)
+    set -l pat "\.?.*\.($extpat)\$"
+    set -l pat_esc "\.?.*\.\($extpat_esc\)\$"
+
+    if command -q fd
+        function _find -a dir -V pat -V _flag_depth
+            if test -z $_flag_depth
+                fd --no-ignore --case-sensitive $pat $dir
+            else
+                fd --no-ignore --case-sensitive $pat $dir --max-depth $_flag_depth
+            end
         end
-        for f in $fs
+    else
+        function _find -a dir -V pat_esc -V _flag_depth
+            if test -z $_flag_depth
+                find $dir -regex $pat_esc
+            else
+                find $dir -maxdepth $_flag_depth -regex $pat_esc
+            end
+        end
+    end
+
+    for d in $argv
+        for f in (_find $d)
             set -l dir (dirname $f)
             set -l base (string trim -l -c'.' (basename $f))
             set -l name (string replace -r ".($extpat)\$" '' $base)
@@ -36,4 +50,6 @@ function coqclean --description 'Delete Coq compilation files'
             end
         end
     end
+
+    functions --erase _find
 end
