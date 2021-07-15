45434c4aa9e371553e5b46106880b14e30e77a89
whonore <wolfhonore@gmail.com>
2021-07-15 14:35:01 -0400

    Reuse version for bitwise GitHub rev

--- nix/bitwise/default.nix
+++ nix/bitwise/default.nix
@@ -1,14 +1,14 @@
 { pkgs ? import <nixpkgs> { } }:
 with pkgs;
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   pname = "bitwise";
   version = "0.42";
 
   src = fetchFromGitHub {
     owner = "mellowcandle";
     repo = "bitwise";
-    rev = "f70b6543981a8d56a567858197a9cfd7668878e6";
+    rev = "${version}";
     sha256 = "154y0sn3z64z56k84ghsazkyihbkaz40hfwxcxdymnhvhh6m9f3g";
   };
 
