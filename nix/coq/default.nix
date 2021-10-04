14f48a8563424d9cfe76afcaed1805a528f1ea98
whonore <wolfhonore@gmail.com>
2021-10-04 11:16:30 -0400

    Simplify coq/default.nix

--- nix/coq/default.nix
+++ nix/coq/default.nix
@@ -1,14 +1,7 @@
 { pkgs ? import <nixpkgs> { }, version }:
 
-let
-  url_8_4 = "https://github.com/NixOS/nixpkgs/archive/18.03.tar.gz";
-  url_master = "https://github.com/coq/coq-on-cachix/tarball/master";
-  coq = "coq_" + builtins.replaceStrings [ "." ] [ "_" ] version;
-  coqpkgs =
-    if version == "8.4" then import (fetchTarball url_8_4) { } else pkgs;
-in if builtins.hasAttr coq coqpkgs then
-  builtins.getAttr coq coqpkgs
-else if version == "master" then
-  import (fetchTarball url_master) { }
+let url_8_4 = "https://github.com/NixOS/nixpkgs/archive/18.03.tar.gz";
+in if version == "8.4" then
+  (import (fetchTarball url_8_4) { }).coq_8_4
 else
-  abort "Invalid version ${version}"
+  pkgs.coq.override ({ inherit version; })
