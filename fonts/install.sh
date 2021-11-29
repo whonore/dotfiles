27c9f02e1ea422a431dd4cc15777150fbaf1848a
whonore <wolfhonore@gmail.com>
2021-11-29 12:09:04 -0500

    Update FiraCode

--- fonts/install.sh
+++ fonts/install.sh
@@ -4,7 +4,7 @@ set -e
 FONTS_ROOT=$HOME/.local/share/fonts
 mkdir -p "$FONTS_ROOT"
 
-version=5.2
+version=6
 zip=Fira_Code_v$version.zip
 curl -fLO https://github.com/tonsky/FiraCode/releases/download/$version/$zip
 unzip -o -q -d "$FONTS_ROOT"/FiraCode $zip
