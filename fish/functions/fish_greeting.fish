f06f0b2195ae13750481cc2a88c5156a31b00171
whonore <wolfhonore@gmail.com>
2022-01-25 10:27:37 -0500

    Set proofaday timeout

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -2,10 +2,10 @@ if command -q proofaday
     function fish_greeting
         alias pyexec='env -u PYTHONNOUSERSITE'
 
-        if not pyexec proofaday 2>/dev/null
+        if not pyexec proofaday -t1 2>/dev/null
             pyexec dproofaday -q start
             pyexec dproofaday -q status -w
-            pyexec proofaday
+            pyexec proofaday -t1
         end
 
         functions -e pyexec
