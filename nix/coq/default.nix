6468e3735ffbcb515454d8e8397e9615c93ebef4
whonore <wolfhonore@gmail.com>
2022-02-02 13:25:05 -0500

    Remove unnecessary parentheses

--- nix/coq/default.nix
+++ nix/coq/default.nix
@@ -4,4 +4,4 @@ let url_8_4 = "https://github.com/NixOS/nixpkgs/archive/18.03.tar.gz";
 in if version == "8.4" then
   (import (fetchTarball url_8_4) { }).coq_8_4
 else
-  pkgs.coq.override ({ inherit version; })
+  pkgs.coq.override { inherit version; }
