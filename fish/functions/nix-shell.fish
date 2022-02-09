4b32068afc4db4fe5fc5fa6a28f32e9c6622968c
whonore <wolfhonore@gmail.com>
2022-02-09 09:32:25 -0500

    Use nix shell instead of nix-shell

--- fish/functions/nix-shell.fish
+++
@@ -1,5 +0,0 @@
-# Redefine nix-shell to use fish
-function nix-shell
-    set -l com "export SHELL=$SHELL; $SHELL"
-    command nix-shell --command "$com" $argv
-end
--- fish/functions/usecoq.fish
+++ fish/functions/usecoq.fish
@@ -1,3 +1,3 @@
 function usecoq --description "Start a shell with the given Coq version." --argument-names coqv
-    nix-shell "$HOME/.dotfiles/nix/coq/shell.nix" -j auto --argstr version $coqv
+    nix shell -f "$HOME/.dotfiles/nix/coq/default.nix" -j auto --cores 0 --argstr version $coqv
 end
