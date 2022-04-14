b1a5e7ced2697b73c3e8ec650c9964182c6ace1b
whonore <wolfhonore@gmail.com>
2022-04-14 15:13:38 -0400

    Factor our toPluginCfg in universal-ctags module

--- nix/modules/universal-ctags.nix
+++ nix/modules/universal-ctags.nix
@@ -6,6 +6,10 @@
 }:
 with lib; let
   cfg = config.programs.universal-ctags;
+
+  toPluginCfg = plugins:
+    concatStringsSep "\n"
+    (map (plug: "--options=${plug}/share/ctags.d/optlib") plugins);
 in {
   options = {
     programs.universal-ctags = {
@@ -23,17 +27,9 @@ in {
     };
   };
 
-  config = let
-    pluginCfg =
-      concatStringsSep "\n"
-      (map (plug: "--options=${plug}/share/ctags.d/optlib") cfg.plugins);
-  in
-    mkIf cfg.enable {
-      home.packages = [pkgs.universal-ctags] ++ cfg.plugins;
-      xdg.configFile."ctags/config.ctags".text =
-        ''
-          ${pluginCfg}
-        ''
-        + cfg.extraConfig;
-    };
+  config = mkIf cfg.enable {
+    home.packages = [pkgs.universal-ctags] ++ cfg.plugins;
+    xdg.configFile."ctags/config.ctags".text =
+      toPluginCfg cfg.plugins + cfg.extraConfig;
+  };
 }
