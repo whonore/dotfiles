51e81bfc97584b17264c83d275d01bc473ef0024
whonore <wolfhonore@gmail.com>
2022-06-09 14:31:39 -0400

    Update packages

--- nix/packages.nix
+++ nix/packages.nix
@@ -1,13 +1,13 @@
 pkgs:
 with pkgs; [
   ## app
-  drawio               # 18.0.6
+  drawio               # 19.0.2
   ## cli
   bat                  # 0.21.0
   clang-tools          # 13.0.1
   delta                # 0.13.0
   exa                  # 0.10.1
-  fd                   # 8.3.2
+  fd                   # 8.4.0
   fzf                  # 0.30.0-man
   ripgrep              # 13.0.0
   rlwrap               # 0.45.2
@@ -15,14 +15,14 @@ with pkgs; [
   xclip                # 0.13
   zoxide               # 0.8.1
   ## coq
-  coq_8_15             # 8.15.1
+  coq_8_15             # 8.15.2
   ## fmt
-  alejandra            # 1.3.0
+  alejandra            # 1.4.0
   bibclean             # 3.06
-  cppcheck             # 2.7.5-man
+  cppcheck             # 2.8
   nixfmt               # 0.5.0
-  shellcheck           # 0.8.0-doc
-  shfmt                # 3.5.0
+  shellcheck           # 0.8.0-bin
+  shfmt                # 3.5.1
   ## fonts
   fira-code            # 6.2
   ## misc
@@ -37,17 +37,17 @@ with pkgs; [
   asciidoc             # 9.1.0
   lastpass-cli         # 1.3.3
   ## rust
-  rust-analyzer        # 2022-05-02
+  rust-analyzer        # 2022-05-17
   rustup               # 1.24.3
   ## shell
   fish                 # 3.4.1
   pash                 # 0.7
   ## vim
   coq-ctags            # 0.0
-  universal-ctags      # 5.9.20220220.0
+  universal-ctags      # 5.9.20220529.0
   vim                  # 8.2.4700
   ## xmonad
   dmenu                # 5.1
-  xmobar               # 0.42
+  xmobar               # 0.43
   xmonad-with-packages # 9.0.2
 ]
