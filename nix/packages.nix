1e10ddf00b185ecca4a751661f4478005abebe11
whonore <wolfhonore@gmail.com>
2022-08-07 16:44:40 -0400

    Update packages

--- nix/packages.nix
+++ nix/packages.nix
@@ -14,14 +14,14 @@ in
     builtins.filter supports
     [
       ## app
-      drawio               # 19.0.3
+      drawio               # 20.2.3
       ## cli
       bat                  # 0.21.0
       clang-tools          # 13.0.1
       delta                # 0.13.0
       exa                  # 0.10.1-man
       fd                   # 8.4.0
-      fzf                  # 0.30.0-man
+      fzf                  # 0.32.0
       ripgrep              # 13.0.0
       rlwrap               # 0.45.2
       rm-improved          # 0.13.0
@@ -30,11 +30,11 @@ in
       ## coq
       coq_8_15             # 8.15.2
       ## fmt
-      alejandra            # 1.5.0
+      alejandra            # 2.0.0
       bibclean             # 3.06
-      cppcheck             # 2.8
+      cppcheck             # 2.8.2-man
       nixfmt               # 0.5.0
-      shellcheck           # 0.8.0-doc
+      shellcheck           # 0.8.0-bin
       shfmt                # 3.5.1
       ## fonts
       fira-code            # 6.2
@@ -43,24 +43,24 @@ in
       openconnect          # 9.01
       peridot              # 0.1.1
       ## nix
-      cachix               # 0.7.1
+      cachix               # 0.8.1
       # NOTE: see https://github.com/NixOS/nixpkgs/issues/38991
-      glibcLocales         # 2.34-210
+      glibcLocales         # 2.35-163
       ## qutebrowser
       asciidoc             # 10.2.0
       lastpass-cli         # 1.3.3
       ## rust
-      rust-analyzer        # 2022-06-13
-      rustup               # 1.24.3
+      rust-analyzer        # 2022-08-01
+      rustup               # 1.25.1
       ## shell
-      fish                 # 3.5.0
+      fish                 # 3.5.1
       pash                 # 0.7
       ## vim
       coq-ctags            # 0.0
-      universal-ctags      # 5.9.20220529.0
+      universal-ctags      # 5.9.20220710.0
       vim                  # 9.0.0048
       ## xmonad
       dmenu                # 5.1
-      xmobar               # 0.43
+      xmobar               # 0.44.1
       xmonad-with-packages # 9.0.2
     ]
