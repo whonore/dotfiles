b10e3acdbbb1d5fb71899e37f5994798be3b179d
whonore <wolfhonore@gmail.com>
2022-11-21 14:00:55 -0500

    Update packages

--- nix/packages.nix
+++ nix/packages.nix
@@ -23,19 +23,19 @@ in
       clang-tools          # 14.0.6
       delta                # 0.14.0
       exa                  # 0.10.1
-      fd                   # 8.4.0
-      fzf                  # 0.34.0
+      fd                   # 8.5.3
+      fzf                  # 0.35.1
       ripgrep              # 13.0.0
-      rlwrap               # 0.45.2
+      rlwrap               # 0.46
       rm-improved          # 0.13.0
       xclip                # 0.13
       zoxide               # 0.8.3
       ## coq
       coq_8_15             # 8.15.2
       ## fmt
-      alejandra            # 3.0.0+20221003.ba0b931
+      alejandra            # 3.0.0+20221113.378cde9
       bibclean             # 3.06
-      cppcheck             # 2.9
+      cppcheck             # 2.9.2
       nixfmt               # 0.5.0
       shellcheck           # 0.8.0
       shfmt                # 3.5.1
@@ -53,16 +53,16 @@ in
       asciidoc             # 10.2.0
       lastpass-cli         # 1.3.3
       ## rust
-      rust-analyzer        # 2022-10-10
+      rust-analyzer        # 2022-10-31
       rustup               # 1.25.1
       ## shell
       fish                 # 3.5.1
       ## vim
       coq-ctags            # 0.0.0
-      universal-ctags      # 5.9.20220814.0
+      universal-ctags      # 5.9.20221106.0
       vim                  # 9.0.0399
       ## xmonad
-      dmenu                # 5.1
+      dmenu                # 5.2
       xmobar               # 0.44.2
       xmonad-with-packages # 9.0.2
     ]
