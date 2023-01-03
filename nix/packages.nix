f7a5f10de0c01d230542f0d0cd7b2d56242e0690
whonore <wolfhonore@gmail.com>
2023-01-03 09:50:09 -0500

    Update packages

--- nix/packages.nix
+++ nix/packages.nix
@@ -21,9 +21,9 @@ in
       ## cli
       bat                  # 0.22.1
       clang-tools          # 14.0.6
-      delta                # 0.14.0
+      delta                # 0.15.1
       exa                  # 0.10.1
-      fd                   # 8.5.3
+      fd                   # 8.6.0
       fzf                  # 0.35.1
       ripgrep              # 13.0.0
       rlwrap               # 0.46
@@ -35,10 +35,10 @@ in
       ## fmt
       alejandra            # 3.0.0+20221113.378cde9
       bibclean             # 3.06
-      cppcheck             # 2.9.2
+      cppcheck             # 2.9.3
       nixfmt               # 0.5.0
-      shellcheck           # 0.8.0
-      shfmt                # 3.5.1
+      shellcheck           # 0.9.0
+      shfmt                # 3.6.0
       ## fonts
       fira-code            # 6.2
       ## misc
@@ -46,14 +46,14 @@ in
       openconnect          # 9.01
       peridot              # 0.1.1
       ## nix
-      cachix               # 1.0.1
+      cachix               # 1.1
       # NOTE: see https://github.com/NixOS/nixpkgs/issues/38991
       glibcLocales         # 2.35-163
       ## qutebrowser
       asciidoc             # 10.2.0
       lastpass-cli         # 1.3.3
       ## rust
-      rust-analyzer        # 2022-10-31
+      rust-analyzer        # 2022-12-26
       rustup               # 1.25.1
       ## shell
       fish                 # 3.5.1
