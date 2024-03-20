1ebe6ab405a0df3643e8a32babfde3e9816ccf5c
whonore <wolfhonore@gmail.com>
2024-03-20 09:48:09 -0400

    Update nix packages

--- nix/packages.nix
+++ nix/packages.nix
@@ -26,44 +26,44 @@ builtins.filter supports [
   ## cli
   bat                  # 0.24.0
   clang-tools          # 16.0.6
-  delta                # 0.16.5
-  eza                  # 0.17.0
+  delta                # 0.17.0
+  eza                  # 0.18.7
   fd                   # 9.0.0
-  fzf                  # 0.44.1
-  ripgrep              # 14.0.3
+  fzf                  # 0.47.0
+  ripgrep              # 14.1.0
   rlwrap               # 0.46.1
   rm-improved          # 0.13.0
   xclip                # 0.13
-  zoxide               # 0.9.2
+  zoxide               # 0.9.4
   ## fmt
   bibclean             # 3.07
-  cppcheck             # 2.12.1
+  cppcheck             # 2.13.4
   nixfmt               # 0.6.0
   shellcheck           # 0.9.0
-  shfmt                # 3.7.0
+  shfmt                # 3.8.0
   ## fonts
   fira-code            # 6.2
   ## misc
   peridot              # 0.1.1
   ## nix
-  cachix               # 1.6.1
+  cachix               # 1.7
   # NOTE: see https://github.com/NixOS/nixpkgs/issues/38991
   glibcLocales         # 2.38-27
   ## qutebrowser
   asciidoc             # 10.2.0
-  bitwarden-cli        # 2023.12.0
+  bitwarden-cli        # 2024.2.1
   ## rust
   rustup               # 1.26.0
   ## shell
-  fish                 # 3.6.4
+  fish                 # 3.7.0
   ## verification
   coq_8_17             # 8.17.1
-  dafny                # 4.4.0
+  dafny                # 4.5.0
   ## vim
   coq-ctags            # 0.0.0
   solidity-ctags       # 0.0.2
-  universal-ctags      # 6.0.0
-  vim                  # 9.0.1291
+  universal-ctags      # 6.1.0
+  vim                  # 9.0.2190
   ## xmonad
   dmenu                # 5.2
   xmobar               # 0.47.2
