b0bdd74a659cd6ebe3de6b108fba8113751a8a13
whonore <wolfhonore@gmail.com>
2026-05-16 11:22:29 -0700

    Update nix packages

--- nix/packages.nix
+++ nix/packages.nix
@@ -24,48 +24,48 @@ let
 in with pkgs;
 builtins.filter supports [
   ## cli
-  bat                  # 0.24.0
-  clang-tools          # 17.0.6
-  delta                # 0.17.0
-  eza                  # 0.18.10
-  fd                   # 9.0.0
-  fzf                  # 0.50.0
-  ripgrep              # 14.1.0
-  rlwrap               # 0.46.1
+  bat                  # 0.26.1
+  clang-tools          # 21.1.8
+  delta                # 0.19.2
+  eza                  # 0.23.4
+  fd                   # 10.4.2
+  fzf                  # 0.72.0
+  ripgrep              # 15.1.0
+  rlwrap               # 0.48
   rm-improved          # 0.13.1
   xclip                # 0.13
-  zoxide               # 0.9.4
+  zoxide               # 0.9.9
   ## fmt
   bibclean             # 3.07
-  cppcheck             # 2.13.4
-  nixfmt               # 0.6.0
-  shellcheck           # 0.10.0
-  shfmt                # 3.8.0
+  cppcheck             # 2.18.3
+  nixfmt               # 1.2.0
+  shellcheck           # 0.11.0
+  shfmt                # 3.13.1
   ## fonts
   fira-code            # 6.2
   ## misc
   peridot              # 0.1.1
   ## nix
-  cachix               # 1.7
+  cachix               # 1.11.1
   # NOTE: see https://github.com/NixOS/nixpkgs/issues/38991
-  glibcLocales         # 2.39-5
+  glibcLocales         # 2.42-61
   ## qutebrowser
-  asciidoc             # 10.2.0
-  bitwarden-cli        # 2024.3.1
+  asciidoc             # 10.2.1
+  bitwarden-cli        # 2026.4.1
   ## rust
-  rustup               # 1.26.0
+  rustup               # 1.29.0
   ## shell
-  fish                 # 3.7.1
+  fish                 # 4.7.1
   ## verification
   coq_8_17             # 8.17.1
-  dafny                # 4.6.0
+  dafny                # 4.11.0
   ## vim
   coq-ctags            # 0.0.0
   solidity-ctags       # 0.0.2
-  universal-ctags      # 6.1.0
-  vim                  # 9.1.0346
+  universal-ctags      # 6.2.1
+  vim                  # 9.2.0043
   ## xmonad
-  dmenu                # 5.3
-  xmobar               # 0.47.4
-  xmonad-with-packages # 9.6.4
+  dmenu                # 5.4
+  xmobar               # 0.50
+  xmonad-with-packages # 9.10.3
 ]
